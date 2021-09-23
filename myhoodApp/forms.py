3e2aee026313336186de00bf5ddc95004db5565c
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-09-23 16:20:22 +0300

    App forms

---
+++ myhoodApp/forms.py
@@ -0,0 +1,93 @@
+
+from . models import Profile, Business,Myhood, PolicePosts, HealthFacilities, UserPost
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.forms import widgets
+
+
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+      
+        def __init__(self, *args, **kwargs):
+            super(UserRegistrationForm, self).__init__(*args, **kwargs)
+
+            for fieldname in ['username', 'password1', 'password2']:
+                self.fields[fieldname].help_text = None
+
+        fields = ('first_name', 'last_name', 'email','username',  'password1', 'password2')
+        widgets = {
+            'first_name':forms.TextInput(attrs = {'class':'form-control names', 'placeholder':"First Name", 'label': 'First Name'}),
+            'last_name':forms.TextInput(attrs = {'class':'form-control names', 'placeholder':"Second Name", 'label': 'Second Name'}),
+            'email':forms.TextInput(attrs = {'class':'form-control names', 'placeholder':"Email Address", 'label': 'Email Address'}),
+            'username':forms.TextInput(attrs = {'class':'form-control names', 'placeholder':"Username", 'label': 'Username'}),
+            'password1':forms.PasswordInput(attrs = {'class':'form-control ', 'placeholder':"Password", 'label': 'Password'}),
+            'password2':forms.PasswordInput(attrs = {'class':'form-control', 'type':'password', 'placeholder':"Confirm Password", 'label': 'Confirm Password'}),
+        }
+
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = [ 'profile_pic','family_name', 'bio','gender','general_location', 'mobile','hood' ]
+        widgets = {
+            'profile_pic': forms.FileInput(attrs={'class':'form-control'}),
+            'family_name':forms.TextInput(attrs={'class': 'form-control'}),
+            'bio':forms.Textarea(attrs={'class': 'form-control'}),
+            'gender':forms.Select(attrs={'class': 'form-control'}),
+            'general_location':forms.TextInput(attrs={'class': 'form-control'}),
+            'hood':forms.Select(attrs={'class': 'form-control'}),
+
+
+            
+        }
+
+
+class HoodMemberPostForm(forms.ModelForm):
+    class Meta:
+        model = UserPost
+        fields = ('title', 'post')
+
+
+
+
+
+class UpdatePostForm(forms.ModelForm):
+    class Meta:
+        model = UserPost
+        fields = [  'post_pic', 'title','post']
+        widgets = {
+            'project_pic':forms.FileInput(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class':'form-control'}), 
+            'post':forms.Textarea(attrs={'class': 'form-control'}),
+            
+            
+        
+        }
+
+class RegisterBizForm(forms.ModelForm):
+    class Meta:
+        model =  Business
+        fields = [ 'biz_image', 'name', 'description','phone','email']
+        widgets = {
+            'biz_image':forms.FileInput(attrs={'class': 'form-control'}),
+            'name': forms.TextInput(attrs={'class':'form-control'}), 
+            'description':forms.TextInput(attrs={'class': 'form-control'}),
+            'phone':forms.TextInput(attrs={'class': 'form-control'}),
+            'email':forms.TextInput(attrs={'class': 'form-control'}),
+
+            
+            
+        
+        }
+
+
+        
+
+
+  
+
+
+
