928f1ef4585cf66f8af67ced7aeb58e71c3e0b03
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-09-23 16:22:17 +0300

    JS script

---
+++ static/js/script.js
@@ -0,0 +1,31 @@
+
+
+
+
+// Profile
+function showPosts() {
+    document.getElementById("posts").style.display = " block";
+    document.getElementById("posts").style.display = " flex";  
+    document.getElementById("business").style.display = "none"; 
+    document.getElementById("neighbors").style.display = "none";
+
+
+}
+
+function showBusiness() {
+    document.getElementById("business").style.display = " block";
+    document.getElementById("posts").style.display = "none";
+    document.getElementById("neighbors").style.display = "none";
+
+
+
+}
+
+function showNeighbors() {
+  document.getElementById("neighbors").style.display = " block";
+  document.getElementById("posts").style.display = "none";
+  document.getElementById("business").style.display = "none";
+
+
+}
+
