dfe44d894c4aaea5a6035587cc4223d708dfe309
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-09-24 11:48:23 +0300

    app deployment

--- myhood/settings.py
+++ myhood/settings.py
@@ -129,7 +129,7 @@ if config('MODE')=="dev":
 else:
    DATABASES = {
        'default': dj_database_url.config(
-           default=config('DATABASE_URL')
+           default=config('MYHOOD_DB')
        )
    }
 
