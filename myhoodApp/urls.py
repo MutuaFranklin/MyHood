e46382bfbb44e1ad26ee1e1115f40ca628421ffd
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-09-24 10:33:04 +0300

    new profile urls

--- myhoodApp/urls.py
+++ myhoodApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from .views  import UpdateProfileView
+from .views  import UpdateProfileView, NewProfileUpdateView
 
 
 
@@ -14,6 +14,7 @@ urlpatterns = [
     path('home', views.home,name='home'),
     path('search/', views.search, name='search_business'),
     path('<username>/', views.userProfile, name='userProfile'),
+    path('update-new-profile/<int:pk>', NewProfileUpdateView.as_view(), name='new-profile'),
     path('update/<int:pk>', UpdateProfileView.as_view(), name='update-profile'),
     path('health-facility', views.registerHealthFacility,name='health_facility'),
 
